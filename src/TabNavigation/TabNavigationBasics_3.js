4ab679e488f6003eaeaf3b446487d48fce037d9b
chenlw <>
2018-08-30 09:49:52 +0800

    TabNavigation练习

--- src/TabNavigation/TabNavigationBasics_3.js
+++ src/TabNavigation/TabNavigationBasics_3.js
@@ -7,8 +7,26 @@ import {
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {
-    createBottomTabNavigator
+    createBottomTabNavigator,
+    createStackNavigator
 } from 'react-navigation';
+
+
+
+class DetailsScreen extends React.Component{
+    render(){
+        return (
+            <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                <Text>Details</Text>
+                <Button title="Go to Settings" onPress={()=>{
+                    this.props.navigation.navigate("Settings",{
+                        param:"param from Details"
+                    });
+                }}></Button>
+            </View>
+        );
+    }
+}
 
 
 class HomeScreen extends React.Component {
@@ -55,21 +73,21 @@ class SettingsScreen extends React.Component {
 }
 
 
-/**    }
+const HomeStack = createStackNavigator({
+    Home:HomeScreen,
+    Details:DetailsScreen,
+});
+
+const SettingsStack = createStackNavigator({
+    Settings:SettingsScreen,
+    Details:DetailsScreen
+});
 
 
- 自定义外观
- 这与如何自定义 stack navigator 类似 — 有些属性可以在初始化 tab navigator 时设置，有些可以在每个页面的navigationOptions中自定义。
-
- （1）tabBarIcon是navigationOptions上的一个属性，所以我们知道我们可以在我们的页面上使用它，
- 但在这种情况下，选择将它放在createBottomTabNavigator的配置中，是为了方便集中配置图标。
- （2）tabBarIcon是一个赋予了focused 这个 state 和tintColor的函数。 如果你在配置中进一步查看，您将看到tabBarOptions、activeTintColor和inactiveTintColor。 这些默认为 iOS 平台的默认值，但您可以在这里更改它们。 传递给tabBarIcon的tintColor取决于focused这个 state （判断该 Tab 是否获得了焦点），可以是活动，也可以是非活动状态
-
- */
 export default createBottomTabNavigator(
     {
-        Home: HomeScreen,
-        Settings: SettingsScreen,
+        Home: HomeStack,
+        Settings: SettingsStack,
     },
     {
         navigationOptions: ({ navigation }) => ({
