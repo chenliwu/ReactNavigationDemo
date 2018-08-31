c9d0ad3f3dad426d943449c6cd2251efde57bbda
chenlw <>
2018-08-31 13:46:27 +0800

    createBottomTabNavigator API练习

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { AppRegistry } from 'react-native';
 //import TabNavigatorBasics from './src/TabNavigation/TabNavigationBasics';
 
 import createStackNavigatorAPI_Test from './src/StackNavigation/createStackNavigatorAPI_Test';
-import createTabNavigatorAPI_Test from './src/TabNavigation/createTabNavigatorAPI_Test';
+import createBottomTabNavigatorAPI_Test from './src/TabNavigation/createBottomTabNavigatorAPI_Test';
 
 import NavigationProp_Test from './src/NavigationProp_Test/NavigationPropTest_1';
 import DeawerNavigator from './src/DrawerNavigation/DrawerNavigation_1';
@@ -15,4 +15,4 @@ import MyTabNavigator from './src/MyTabNavigator/MyTabNavigator';
 //AppRegistry.registerComponent('ReactNavigationDemo', () => App);
 //AppRegistry.registerComponent('ReactNavigationDemo', () => StackNavigatorBasics);
 //AppRegistry.registerComponent('ReactNavigationDemo', () => DeawerNavigator);
-AppRegistry.registerComponent('ReactNavigationDemo', () => createTabNavigatorAPI_Test);
+AppRegistry.registerComponent('ReactNavigationDemo', () => createBottomTabNavigatorAPI_Test);
--- src/TabNavigation/createBottomTabNavigatorAPI_Test.js
+++ src/TabNavigation/createBottomTabNavigatorAPI_Test.js
@@ -33,6 +33,9 @@ class HomeScreen extends React.Component {
                         param: "parameter from home"
                     })
                 }}>
+                <Button title="显示主页Tab" onPress={()=>{
+                    
+                }}></Button>
 
                 </Button>
             </View>
@@ -54,37 +57,71 @@ class SettingsScreen extends React.Component {
                         param: 'parameter from Settings'
                     })
                 }}>
-
+            
                 </Button>
             </View>
         );
     }
 }
 
+
+class PersonalScreen extends React.Component{
+
+    render(){
+        return (
+            <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                <Text>Personal!</Text>
+            </View>
+        );
+    }
+
+}
+
+/**
+ * createBottomTabNavigator(RouteConfigs, BottomTabNavigatorConfig);
+    (1)RouteConfigs
+路由配置对象是从路由名称到路由配置的映射，它告诉导航器该路由要呈现的内容，请参阅示例 stack navigator。
+    (2)BottomTabNavigatorConfig
+ */
 export default createBottomTabNavigator(
     {
-        Home: HomeScreen,
-        Settings: SettingsScreen,
+        Home: {
+            screen: HomeScreen,
+            navigationOptions: {
+                //title:"主页",       //可用作headerTitle和tabBarLabel的后备的通用标题。
+                tabBarVisible: true, //true或false用于显示或隐藏标签栏，如果未设置，则默认为true。
+                tabBarLabel: "主页", //标签栏或 React 元素中显示的选项卡的标题字符串
+            }
+        },
+        Settings: {
+            screen: SettingsScreen,
+            navigationOptions: {
+                tabBarVisible: true,
+                tabBarLabel: "设置",
+            },
+        },
+        Personal:{
+            screen:PersonalScreen,
+            navigationOptions:{
+                tabBarLabel:"我的",
+            }
+        }
     },
     {
-        navigationOptions: ({ navigation }) => ({
-            tabBarIcon: ({ focused, tintColor }) => {
-                const { routeName } = navigation.state;
-                let iconName;
-                if (routeName === 'Home') {
-                    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
-                } else if (routeName === 'Settings') {
-                    iconName = `ios-options${focused ? '' : '-outline'}`;
-                }
-
-                // You can return any component that you like here! We usually use an
-                // icon component from react-native-vector-icons
-                return <Ionicons name={iconName} size={25} color={tintColor} />;
-            },
-        }),
         tabBarOptions: {
             activeTintColor: 'tomato',  //label和icon的背景色，活跃状态下
             inactiveTintColor: 'gray',  //label和icon的背景色，不活跃状态下
+            //activeBackgroundColor:'blue',   //活动选项卡的背景色。
+            //inactiveBackgroundColor:'red',  //非活动选项卡的背景色。
+            labelStyle: {    //选项卡标签的样式对象。
+                fontSize: 16,
+            },
+            tabStyle: {  //选项卡的样式对象。
+
+            },
+            style: {    //选项卡栏的样式对象。
+                //backgroundColor: 'blue',
+            },
         },
     }
 );
